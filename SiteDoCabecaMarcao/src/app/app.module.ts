bf14889f27318d6c54a75a91eda72029c8234569
LucieneFreitas <luciene_freitas@yahoo.com.br>
2023-10-10 22:02:40 -0300

    bootstrap

--- SiteDoCabecaMarcao/src/app/app.module.ts
+++ SiteDoCabecaMarcao/src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './componentes/principal/principal/principal.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [PrincipalComponent],
+
   providers: [],
   bootstrap: [AppComponent]
 })
